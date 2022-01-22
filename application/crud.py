27c3328fbd061280a9ca2442247b8f4e8c6b6423
Mohamed Hussien <eng.mohamedhussien1996@gmail.com>
2022-01-22 18:31:18 +0200

    add search functionality to get applications by client name or phone

--- application/crud.py
+++ application/crud.py
@@ -19,18 +19,36 @@ def get_application(db: Session, id: int):
     return db.query(models.Application).filter(models.Application.id == id).first()
 
 
-def get_applications(db: Session, state: schemas.StateEnum, page: int):
+def get_applications(db: Session, phone: str, name: str, state: schemas.StateEnum, page: int):
     offset = 0
     limit = 25
 
     if page:
         offset = (page - 1) * 25
 
-    if not state:
-        return db.query(models.Application).offset(offset).limit(limit).all()
+    if not phone:
+        if name:
+            if not state:
+                return db.query(models.Application).filter(models.Application.client_name.contains(name))\
+                                                   .offset(offset).limit(limit).all()
+            else:
+                return db.query(models.Application).filter(models.Application.client_name.contains(name),\
+                                                           models.Application.state == state)\
+                                                   .offset(offset).limit(limit).all()
+        else:
+            if not state:
+                return db.query(models.Application).offset(offset).limit(limit).all()
+            else:
+                return db.query(models.Application).filter(models.Application.state == state)\
+                                                   .offset(offset).limit(limit).all()
     else:
-        return db.query(models.Application).filter(models.Application.state == state)\
-               .offset(offset).limit(limit).all()
+        if not state:
+            return db.query(models.Application).filter(models.Application.client_phone == phone)\
+                                               .offset(offset).limit(limit).all()
+        else:
+            return db.query(models.Application).filter(models.Application.client_phone == phone,\
+                                                       models.Application.state == state)\
+                                               .offset(offset).limit(limit).all()
 
 
 def update_application(db: Session, id: int, request: schemas.ApplicationUpdate):
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import List, Optional
-from fastapi import FastAPI, status, Depends, HTTPException, Response
+from fastapi import FastAPI, status, Depends, HTTPException, Response, Query
 from sqlalchemy.orm import Session
 import uvicorn
 
@@ -22,12 +22,14 @@ def get_db():
 
 @app.post('/application', status_code=status.HTTP_201_CREATED)
 def add_application(application: schemas.ApplicationBase, db: Session=Depends(get_db)):
+    
     crud.create_application(db, application)
     return {'status': 'Application has been added successfully'}
 
 
 @app.get('/application/{id}', response_model=schemas.Application)
 def get_application(id: int, db: Session=Depends(get_db)):
+    
     application = crud.get_application(db, id)
     
     if not application:
@@ -37,19 +39,27 @@ def get_application(id: int, db: Session=Depends(get_db)):
     return application
 
 
-@app.get('/application', response_model=List[schemas.Application])
-def get_applications(state: Optional[schemas.StateEnum] = None, page: Optional[int] = None, db: Session=Depends(get_db)):
-    return crud.get_applications(db, state, page)
+@app.get('/applications', response_model=List[schemas.Application])
+def get_applications(phone: Optional[str] = Query(None, min_length = 11, max_length = 12, regex = "2?\d{11}"),
+                     name : Optional[str] = None,
+                     state: Optional[schemas.StateEnum] = None, page: Optional[int] = None, 
+                     db   : Session=Depends(get_db)):
+    '''
+        phone: supported formats with 2 leading phone number (prefix) or without
+    '''    
+    return crud.get_applications(db, phone, name, state, page)
 
 
 @app.put('/application/{id}', status_code=status.HTTP_202_ACCEPTED)
 def update_application(id: int, request: schemas.ApplicationUpdate, db: Session=Depends(get_db)):
     crud.update_application(db, id, request)
+    
     return {'status': f'Application with id {id} has been updated successfully'}
 
 
 @app.delete('/application/{id}')
 def delete_application(id: int, db: Session=Depends(get_db)):
+    
     crud.delete_application(db, id)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
